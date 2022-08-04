3ebb1b1838dfb72322a9fce8d17dcee42a667a0f
yuvemaq <78762744+yuvemaq@users.noreply.github.com>
2022-08-04 17:42:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-const menu = document.querySelector('#mobile-menu')
-const menuULS= document.querySelector('.primarynav__menu')
+const navToggle = () => {
+    const menu = document.querySelector('#mobile-menu');
+    const menuULS= document.querySelector('.primarynav__menu');
 
-// Show our mobile menu
-const mobileMenu = () => {
-    menu.classList.toggle('is-active')
-    menuULS.classList.toggle('active')
+    menu.addEventListener('click', () => {
+        menu.classList.toggle('is-active');
+        menuULS.classList.toggle('active');
+    }); 
 }
 
-//Event listener
-menu.addEventListener('click', mobileMenu);
+const navToggleAbout = () => {
+    const aboutmenu = document.querySelector('#mobile-menu-aboutme');
+    const aboutmenuULS= document.querySelector('.navaboutme__menu');
+
+    aboutmenu.addEventListener('click', () => {
+        aboutmenu.classList.toggle('is-active');
+        aboutmenuULS.classList.toggle('active');
+    }); 
+}
+
+navToggle();
+navToggleAbout();
+
+$(document).ready(function(){
+	$('a.#mobile-menu-aboutme').click(function(e){
+		$('div.aboutme_container, nav.nav-aboutme, a.navaboutme__toggle').toggleClass('toggled');
+		e.preventDefault();
+	});//target-burger-click
+});//doc-rdy
